814b4a14aef9a8387c70343d73fb2dca1c2774c8
radoslawc <radoslawc@gmail.com>
2014-09-04 14:23:25 +0200

    minor fixes

--- mk.sh
+++ mk.sh
@@ -11,7 +11,11 @@ wget -O tmux-${TMUX_VERSION}.tar.gz  "http://downloads.sourceforge.net/project/t
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
 wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
  
-[[ -f "/etc/redhat-release" ]] || sudo yum -y install glibc-static.x86_64 || su -c "yum -y install glibc-static.x86_64 "
+if [ -f "/etc/redhat-release" ]; then 
+
+sudo yum -y install glibc-static.x86_64 || su -c "yum -y install glibc-static.x86_64 "
+
+fi
 
 tar xvzf libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
@@ -36,3 +40,8 @@ cd ..
  
 rm -rf $HOME/tmux_tmp
  
+echo "all done, have fun"
+
+echo "you can find tmux in $HOME/local/bin"
+
+
